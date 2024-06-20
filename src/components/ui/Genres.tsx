8cefe169fdb9204a3230520aa343d4b9267298f3
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-06-19 23:10:50 -0700

    videoCards ....

--- src/components/ui/Genres.tsx
+++ src/components/ui/Genres.tsx
@@ -1,13 +1,68 @@
 import VideoCard from './VideoCard';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 
 const Genres = () => {
+  var settings = {
+    className: `slider`,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    infinite: true,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 4000,
+    cssEase: 'linear',
+
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 774,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className=" mx-16 h-max flex items-start flex-col gap-y-6">
+    <div className="mx-16 pl-8 h-max flex items-start flex-col gap-y-6">
       <div className="text-[2rem] font-bold font-title">Actions</div>
 
-      <div className="flex items-start gap-x-6">
-        <VideoCard />
-        <VideoCard />
+      <div className="h-[22rem] w-full min-[840px]:w-5/6 min-[1024px]:w-full">
+        <Slider {...settings} className="flex items-start gap-x-4">
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+        </Slider>
       </div>
     </div>
   );
--- src/components/ui/VideoCard.tsx
+++ src/components/ui/VideoCard.tsx
@@ -1,6 +1,6 @@
 const VideoCard = () => {
   return (
-    <div className="w-max  p-6 bg-mymuted overflow-hidden rounded-lg shadow-2xl">
+    <div className="w-max  p-6 bg-mymuted overflow-hidden rounded-lg shadow-2xl cursor-pointer">
       <img
         src="https://m.media-amazon.com/images/M/MV5BNmYzMWVjNmQtNjJjNy00M2Y4LTkzZjQtZWQ5NmYzMjRjMDIzXkEyXkFqcGdeQXVyMTM1NjM2ODg1.300_.jpg"
         className="w-[10rem] h-[16rem]  object-cover object-center rounded-lg mb-4"
